0ac57d9f89c097384fc3f7410e10a02743e68d1e
fouad97 <loukrichi21@gmail.com>
2020-04-09 17:36:44 +0100

    added strong attack function

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,7 +1,9 @@
 /* eslint-disable no-unused-vars */
 
 // Set the Attack Value
-const ATTACK_VALUE = 20;
+const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 20;
+const MONSTER_ATTACK_VALUE = 14;
 
 // Set the health numbers
 let fouadLife = 100; // set dynamically by the user
@@ -12,9 +14,37 @@ adjustHealthBars(fouadLife);
 
 function attackHandler() {
 	const monsterDamage = dealMonsterDamage(ATTACK_VALUE);
+	const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
 	monsterLife -= monsterDamage;
-	if (monsterLife <= 0) {
-		console.log("dadasd");
+	playerLife -= playerDamage;
+	if (monsterLife <= 0 && playerLife > 0) {
+		console.log("monster loose");
+	} else if (monsterLife > 0 && playerLife <= 0) {
+		console.log("player loose");
+	} else if (monsterLife <= 0 && playerLife <= 0) {
+		console.log("you have a draw!");
 	}
 }
+
+function strongAttackHandler() {
+	// Hit player life
+	const monsterDamage = dealMonsterDamage(STRONG_ATTACK_VALUE);
+	const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+
+	// Update players life on page
+	monsterLife -= monsterDamage;
+	playerLife -= playerDamage;
+
+	// Determine who is the winner
+	if (monsterLife > 0 && playerLife <= 0) {
+		console.log("Monster Win!");
+	} else if (monsterLife <= 0 && playerLife > 0) {
+		console.log("You Win!");
+	} else if (monsterLife <= 0 && playerLife <= 0) {
+		console.log("You have a draw!");
+	}
+}
+
+// Game Controls Buttons
 attackBtn.addEventListener("click", attackHandler);
+strongAttackBtn.addEventListener("click", strongAttackHandler);
