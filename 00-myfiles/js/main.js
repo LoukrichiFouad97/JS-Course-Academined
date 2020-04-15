93a38dd182a114dca284d58c7ef11de4aed7019a
fouad97 <loukrichi21@gmail.com>
2020-04-15 09:28:37 +0100

    claening the folder structure

--- 00-myfiles/js/main.js
+++ 00-myfiles/js/main.js
@@ -1,1 +1,10 @@
+const func = function (numbers) {
+	let sum = 0;
+	for (const number of numbers) {
+		sum += number;
+	}
+	console.log(sum);
+};
 
+const btn = document.getElementById("tests");
+btn.addEventListener("click", func.bind(this, [10, 52, 232, 56]));
