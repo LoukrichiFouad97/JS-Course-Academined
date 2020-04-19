03700cd7433c8986c6dce265b0fe950c82dd4f5b
fouad97 <loukrichi21@gmail.com>
2020-04-19 17:07:54 +0100

    added arrays built in functions

--- 05-arrays-02-creating-arrays/app.js
+++ 05-arrays-02-creating-arrays/app.js
@@ -1,4 +1,20 @@
-const list = document.querySelectorAll('li');
-console.log(list);
-const toArr = Array.from(list);
-console.log(toArr);
+/**
+ * push(add at the end), pop(remove from the end)
+ * unshift(add at the start), shift(remove from the start)
+ * splice(start cutting at index, how many item to remove, ...items to add)
+ * slice(start cutting at index, End cutting at index)
+ * concat(combine this array)
+ * indexOf(search from the start), lastIndexOf(search from the end)
+ * find((obj) => {true | false}), findIndex() same as find but returns index
+ * includes(value) search in string for value return true or false
+ */
+
+const persons = [51, 2, 3, 564, 8, 5];
+const search = 2;
+const check = persons.includes(search);
+if (check) {
+	const checkIndex = persons.indexOf(search);
+	console.log(`index of ${search} is: ${checkIndex}`);
+} else {
+	console.log(`you search for ${search} not found`);
+}
