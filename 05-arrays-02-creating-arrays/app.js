71bb056d375889f8e21f375a84c17a720104da91
fouad97 <loukrichi21@gmail.com>
2020-04-20 09:51:57 +0100

    dinished array methods

--- 05-arrays-02-creating-arrays/app.js
+++ 05-arrays-02-creating-arrays/app.js
@@ -7,14 +7,23 @@
  * indexOf(search from the start), lastIndexOf(search from the end)
  * find((obj) => {true | false}), findIndex() same as find but returns index
  * includes(value) search in string for value return true or false
+ * forEach((array item, index) => {}) go through each array item
+ * map() same as forEach but it has to return something
+ * sort() sort array
+ * filter() create new filtred array
+ * reduce((previousValue,currentValue), initializeValue)
+ * split(seperator) splits string to array
+ * join(seperator) joins array to string
  */
 
-const persons = [51, 2, 3, 564, 8, 5];
-const search = 2;
-const check = persons.includes(search);
-if (check) {
-	const checkIndex = persons.indexOf(search);
-	console.log(`index of ${search} is: ${checkIndex}`);
-} else {
-	console.log(`you search for ${search} not found`);
-}
+// const numbers = [51, 2, 3, 164, 32, 5, 5];
+// console.log(numbers.filter((item) => item > 50));
+// // Sort from lower to higher
+// console.log(numbers.sort((a, b) => a - b));
+// // sum all prices using reduce
+// console.log(numbers.reduce((a, b) => a + b, 0));
+const address = "Algeria;Msila;AinElMelh;28004";
+const splited = address.split(";");
+console.log(splited);
+const joined = splited.join(",");
+console.log(joined);
