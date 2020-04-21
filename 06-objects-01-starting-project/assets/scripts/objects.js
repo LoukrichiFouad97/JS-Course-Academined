43f04a27b4ae20b1c635a9ee2cf03781ec11b74b
fouad97 <loukrichi21@gmail.com>
2020-04-21 20:06:58 +0100

    added finishing add movie feature

--- 06-objects-01-starting-project/assets/scripts/objects.js
+++ 06-objects-01-starting-project/assets/scripts/objects.js
@@ -1,11 +1,33 @@
 // VARIABLES
 const addMovieBtn = document.getElementById("add-movie-btn");
 const searchBtn = document.getElementById("search-btn");
-const movieList = document.getElementById("movie-list");
-const movies = []
-
+const movies = [];
 
 // FUNCTIONS
+const addMovieToPage = () => {
+	const movieList = document.getElementById("movie-list");
+	if (movies.length === 0) {
+		movieList.classList.remove("visible");
+	} else {
+		movieList.classList.add("visible");
+	}
+	movieList.innerHTML = "";
+	movies.forEach((movie) => {
+		const newMovie = document.createElement("li");
+    let movieInfo = movie.info.movieTitle + " - ";
+    
+		for (const key in movie.info) {
+			if (key !== "movieTitle") {
+				movieInfo += `${key}: ${movie.info[key]}`;
+      }
+      
+      newMovie.textContent = movieInfo;
+			movieList.append(newMovie);
+    }
+    
+	});
+};
+
 const addMovieHandler = () => {
 	const movieTitle = document.getElementById("title").value;
 	const extraName = document.getElementById("extra-name").value;
@@ -23,9 +45,9 @@ const addMovieHandler = () => {
 			movieTitle,
 			[extraName]: extraValue,
 		},
-  };
-  movies.push(newMovie);
-  console.log(movies);
+	};
+	movies.push(newMovie);
+	addMovieToPage();
 };
 
 // EVENTS
