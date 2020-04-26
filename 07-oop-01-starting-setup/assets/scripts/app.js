de39fdf42cd6e7925b1b9a4882295893f5838df6
fouad97 <loukrichi21@gmail.com>
2020-04-26 12:27:56 +0100

    added shopping cart UI

--- 07-oop-01-starting-setup/assets/scripts/app.js
+++ 07-oop-01-starting-setup/assets/scripts/app.js
@@ -1,68 +1,4 @@
-// class Product {
-// 	constructor(title, price, desc, imgUrl) {
-// 		this.title = title;
-// 		this.price = price;
-// 		this.description = desc;
-// 		this.imageUrl = imgUrl;
-// 	}
-// }
-
-// class ProductItem {
-// 	constructor(product) {
-// 		this.product = product;
-// 	}
-
-// 	addToCart() {
-// 		console.log("added to cart...");
-// 		console.log(this.product);
-// 	}
-
-// 	render() {
-// 		const prodEl = document.createElement("li");
-// 		prodEl.setAttribute("class", "product-item");
-// 		prodEl.innerHTML = `
-//         <div>
-//           <img src="${this.product.imageUrl}" alt="${this.product.title}" >
-//           <div class="product-item__content">
-//             <h2>${this.product.title}</h2>
-//             <h3>${this.product.price}</h3>
-//             <p>${this.product.description}</p>
-//             <button>Add to cart</button>
-//           </div>
-//         </div>
-//       `;
-
-// 		const addToCart = prodEl.querySelector("button");
-// 		addToCart.addEventListener("click", this.addToCart.bind(this));
-// 		return prodEl;
-// 	}
-// }
-
-// class ProductList {
-// 	products = [
-// 		new Product("pillow", 19.99, "A Soft pillow", "http://placehold.it/200/10"),
-// 		new Product("A Carpet", 109.99, "A carpet", "http://placehold.it/200/52"),
-// 	];
-
-// 	render() {
-// 		const prodHooks = document.getElementById("app");
-// 		const prodList = document.createElement("ul");
-// 		prodList.setAttribute("class", "product-list");
-
-// 		this.products.forEach((prod) => {
-// 			const productItem = new ProductItem(prod);
-// 			const prodEl = productItem.render();
-// 			prodList.appendChild(prodEl);
-// 		});
-
-// 		prodHooks.appendChild(prodList);
-// 	}
-// }
-
-// const productList = new ProductList();
-// productList.render();
-
-class Products {
+class Product {
 	constructor(title, price, desc, imgUrl) {
 		this.title = title;
 		this.price = price;
@@ -70,23 +6,23 @@ class Products {
 		this.imageUrl = imgUrl;
 	}
 }
+class ShoppingCart {
+	items = [];
 
-class ProductList {
-	products = [
-		new Products("Pillow", 19.99, "soft Pillow", "http://placehold.it/200/50"),
-		new Products("Carpet", 89.99, "Carpet", "http://placehold.it/200/150"),
-	];
+	addProduct(product) {
+		this.items.push(product);
+		this.totalItems = `<h2>Total: \$${1}</h2>`;
+	}
 
 	render() {
-		const prodHook = document.getElementById("app");
-		const prodList = document.createElement("ul");
-		prodList.classList.add("product-list");
-
-		this.products.forEach((prod) => {
-			const productItem = new ProductItem(prod);
-			prodList.append(productItem.render());
-		});
-		prodHook.appendChild(prodList);
+		const cartEl = document.createElement("section");
+		cartEl.className = "cart";
+		cartEl.innerHTML = `
+			<h2>Total: \$${0}</h2>
+			<button>Order Now!</button>
+		`;
+		this.totalItems = cartEl.querySelector("h2");
+		return cartEl;
 	}
 }
 
@@ -95,9 +31,14 @@ class ProductItem {
 		this.product = product;
 	}
 
+	addToCart() {
+		console.log("added to cart...");
+		console.log(this.product);
+	}
+
 	render() {
 		const prodEl = document.createElement("li");
-		prodEl.classList.add("product-item");
+		prodEl.setAttribute("class", "product-item");
 		prodEl.innerHTML = `
         <div>
           <img src="${this.product.imageUrl}" alt="${this.product.title}" >
@@ -108,10 +49,52 @@ class ProductItem {
             <button>Add to cart</button>
           </div>
         </div>
-    `;
+      `;
+
+		const addToCart = prodEl.querySelector("button");
+		addToCart.addEventListener("click", this.addToCart.bind(this));
 		return prodEl;
 	}
 }
 
-const productList = new ProductList();
-productList.render();
+class ProductList {
+	products = [
+		new Product("pillow", 19.99, "A Soft pillow", "http://placehold.it/200/10"),
+		new Product("A Carpet", 109.99, "A carpet", "http://placehold.it/200/52"),
+		new Product("A Carpet", 109.99, "A carpet", "http://placehold.it/200/250"),
+	];
+
+	render() {
+		const prodList = document.createElement("ul");
+		prodList.setAttribute("class", "product-list");
+
+		this.products.forEach((prod) => {
+			const productItem = new ProductItem(prod);
+			const prodEl = productItem.render();
+			prodList.appendChild(prodEl);
+		});
+		return prodList;
+	}
+}
+
+class Shop {
+	render() {
+		const prodHooks = document.getElementById("app");
+		const productList = new ProductList();
+		const prodList = productList.render();
+		const shopCart = new ShoppingCart();
+		const cart = shopCart.render();
+
+		prodHooks.appendChild(cart);
+		prodHooks.appendChild(prodList);
+	}
+}
+
+class App {
+	static init() {
+		const shop = new Shop();
+		shop.render();
+	}
+}
+
+App.init()
