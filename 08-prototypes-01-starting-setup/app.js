4e5db163eaffa485d50cce79cba0f9ef8c6338bd
fouad97 <loukrichi21@gmail.com>
2020-05-04 18:32:10 +0100

    added OOP project basic file and folder structure

---
+++ 08-prototypes-01-starting-setup/app.js
@@ -0,0 +1,35 @@
+// function Car(name, model, year) {
+// 	this.name = name;
+// 	this.model = model;
+// 	this.year = year;
+// 	this.show = function () {
+// 		const { name, model, year } = this;
+// 		console.log(`
+//     car name: ${name}
+//     car model: ${model}
+//     car year: ${year}
+//     `);
+// 	};
+// }
+
+// Car.audi = function () {
+// 	console.log("audi");
+// };
+
+// Car.prototype.speed = function () {
+// 	console.log(this.name + " speed is 307km");
+// };
+
+// const amg = new Car("mercedez amg", "GT63s", 2020);
+// amg.show();
+// amg.speed();
+// console.log(amg);
+
+// function animal() {
+// 	console.log("this is animal");
+// }
+
+// animal.prototype.eat = function () {
+// 	console.log("all animal eat");
+// };
+
